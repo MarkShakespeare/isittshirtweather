19e79e149ab3484e71abb951438f27c5039cafd5
mark shakespeare <markshakespearemusic@gmail.com>
2022-04-29 22:47:09 +0100

    actually fix analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,34 @@
+import Script from 'next/script';
+
 import './styles.css';
 
-export default ({ Component, pageProps }) => <Component {...pageProps} />;
+const GA_MEASUREMENT_ID =
+  process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID;
+
+const App = ({ Component, pageProps }) => (
+  <>
+    <Script
+      strategy="afterInteractive"
+      src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+    />
+    <Script
+      id="gtag-init"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){window.dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${GA_MEASUREMENT_ID}', {
+            page_path: window.location.pathname,
+          });
+          `,
+      }}
+    />
+
+    <Component {...pageProps} />
+  </>
+);
+
+export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,11 @@
-import Script from 'next/script';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import { Home } from 'components';
 
 const queryClient = new QueryClient();
 
-const GA_MEASUREMENT_ID =
-  process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID;
-
 export default () => (
-  <>
-    <Script
-      src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-      strategy="beforeInteractive"
-    />
-    <Script id="google-analytics" strategy="beforeInteractive">
-      {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', ${GA_MEASUREMENT_ID});
-        `}
-    </Script>
-
-    <QueryClientProvider client={queryClient}>
-      <Home />
-    </QueryClientProvider>
-  </>
+  <QueryClientProvider client={queryClient}>
+    <Home />
+  </QueryClientProvider>
 );
